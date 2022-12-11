7d3b69a0d671882b8d267a4dabf1b54a5ec01e7b
igorshchukin <Igor.Shchukin@Bluecrestcapital.com>
2022-12-11 11:15:48 +0100

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +10,10 @@ import { IProduct } from './models/product';
 export class AppComponent implements OnInit {
 
   title = 'SkiNet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50')
-      .subscribe((response: IPagination) => {
-        console.log(response);
-        this.products = response.data;
-      },
-        error => console.error(error));
+
   }
 }
